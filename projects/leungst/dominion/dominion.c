475041a11e23cc1d80c03c0fbbd74e9757571522
salmonavocadocrabstick <iLoveSalmon!1>
2018-11-15 11:09:13 +0800

    Card 3 completed

--- projects/leungst/dominion/dominion.c
+++ projects/leungst/dominion/dominion.c
@@ -14,20 +14,27 @@ void adventurer_effect_bug( int z , int drawntreasure,  struct gameState* state,
         if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
           shuffle(currentPlayer, state);
         }
+
+      // printf("2\n");
           drawCard(currentPlayer, state);
           cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+       
+
         if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
             drawntreasure++;
+
         else{
             temphand[z]=cardDrawn;
             state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
             z++;
           }
    }
+
       while(z-1>0){ //changed to > instead of just >=
            state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
            z=z-1;
          }
+
 }
 
 void council_room_effect_bug(int currentPlayer, struct gameState* state, int handPos){
